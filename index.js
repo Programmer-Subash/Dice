d459554428c1a7036c8bc7328a503ad052ee4699
Programmer-Subash <103889495+Programmer-Subash@users.noreply.github.com>
2022-10-15 00:03:53 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+var player1 = Math.floor(Math.random()*6+1);
+var player2 = Math.floor(Math.random()*6+1);
+var header = document.querySelector("h1");
+var dice1 = document.querySelector(".player_one img");
+var dice2 = document.querySelector(".player_two img");
+
+if(player1>player2){
+    header.textContent = "🚩 Player 1 Wins!";
+}else if(player1<player2){
+    header.textContent = "Player 2 Wins! 🚩";
+}else{
+    header.textContent = "Draw!";
+}
+
+var path1 = "images/dice"+player1+".png";
+dice1.setAttribute("src",path1);
+
+var path2 = "images/dice"+player2+".png";
+dice2.setAttribute("src",path2);
+
+//Bad Because Too Long
+// switch(player1){
+//     case 1:
+//         dice1.setAttribute("src","images/dice1.png");
+//         break;
+//     case 2:
+//         dice1.setAttribute("src","images/dice2.png");
+//         break;
+//     case 3:
+//         dice1.setAttribute("src","images/dice3.png");
+//         break;
+//     case 4:
+//         dice1.setAttribute("src","images/dice4.png");
+//         break;
+//     case 5:
+//         dice1.setAttribute("src","images/dice5.png");
+//         break;
+//     case 6:
+//         dice1.setAttribute("src","images/dice6.png");
+//         break;    
+
+// }
+
+// switch(player2){
+//     case 1:
+//         dice2.setAttribute("src","images/dice1.png");
+//         break;
+//     case 2:
+//         dice2.setAttribute("src","images/dice2.png");
+//         break;
+//     case 3:
+//         dice2.setAttribute("src","images/dice3.png");
+//         break;
+//     case 4:
+//         dice2.setAttribute("src","images/dice4.png");
+//         break;
+//     case 5:
+//         dice2.setAttribute("src","images/dice5.png");
+//         break;
+//     case 6:
+//         dice2.setAttribute("src","images/dice6.png");
+//         break;    
+
+// }
